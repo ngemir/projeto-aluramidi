14a795014a750941bdf45e75ecb5e91b601ae2f5
Emir Takayama <ngemir@gmail.com>
2022-09-01 23:24:50 -0300

    Finalizando projeto

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+
+    const elemento = document.querySelector(seletorAudio);
+    
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    
+    console.log("Elemento não encontrado");
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
-
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
@@ -18,5 +23,15 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 
-    console.log(contador);
+    tecla.onkeydown = function () {
+        if (tecla.code === "Enter" || tecla.code === "Space") {
+            tecla.classList.add("ativa");
+        }
+    };
+
+    tecla.
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove("ativa");
+    };
 }
